b45c1d5319ef85c1915ed2c15f3682fc2c4ed4bf
xieyucheng <648547525@qq.com>
2019-04-10 11:45:20 +0800

    add static

--- src/index.js
+++ src/index.js
@@ -5,41 +5,33 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
   app.quit();
 }
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let mainWindow;
 
+app.commandLine.appendSwitch("--disable-http-cache");
+process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true
+
 const createWindow = () => {
-  // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 600,
+    webPreferences:{
+      nodeIntegration: true,
+    }
   });
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`);
-
+  mainWindow.setTitle('Wallet');
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
-
-  // Emitted when the window is closed.
   mainWindow.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     mainWindow = null;
   });
 };
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
     app.quit();
   }
@@ -52,6 +44,4 @@ app.on('activate', () => {
     createWindow();
   }
 });
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and import them here.
+require('./main/main');
---
+++ src/main/main.js
@@ -0,0 +1,125 @@
+// const { ipcMain } = require('electron')
+// let file = require('../utils/file')
+// let infoparse = require('../parse/walletinfoparse');
+// let blockparse=require('../parse/blockparse');
+// let utxoparse=require('../parse/utxoparse');
+// let http=require('http');
+
+// ipcMain.on('save', function (event, data) {
+//     file.save(data);
+// })
+
+// ipcMain.on('create', function (event, data) {
+//     let addr = file.create(data);
+//     if (addr) {
+//         event.sender.send('replycreate', addr);
+//     }
+// })
+
+// ipcMain.on('block',function(event,data){
+//     var height=20299;
+//     var hash='00...';
+//     var URL = 'http://raw0.nb-chain.net/txn/state/block?&hash=0000000000000000000000000000000000000000000000000000000000000000&hi=20299'
+    
+//     console.log('URL:', URL);
+//     http.get(URL, function (req) {
+//         req.headers = {
+//             'Content-Type': 'application/x-www-form-urlencoded',
+//         }
+//         req.timeout = 30;
+//         var arr = [];
+//         req.on('data', function (chunk) {
+//             arr.push(chunk);
+//         });
+//         req.on('end', function () {
+//             var b = arr[0];
+//             for (var i = 1; i < arr.length; i++) {
+//                 b0 = Buffer.concat(b0, arr[i]);
+//             }
+//             console.log('b:', b, b.length, b.toString('hex'));
+//             var block=blockparse.parse(b);
+//             // var obj=p.parse(b);
+//             // console.log('obj:',obj);
+//             event.sender.send('replyblock',block);
+//         });
+//     });
+
+
+// })
+
+// ipcMain.on('info', function (event, data) {
+//     console.log('data:', data);
+//     let account = 'addr1';
+//     let password = 'xieyc';
+//     let pv = false;
+//     let pb = false;
+//     let after = 0;
+//     let before = 0;
+//     let address = '';
+//     var addr = file.readAccount(account, password);
+
+//     var URL = 'http://raw0.nb-chain.net/txn/state/account?addr=1118Mi5XxqmqTBp7TnPQd1Hk9XYagJQpDcZu6EiGE1VbXHAw9iZGPV&uock=0&uock2=0'
+    
+//     console.log('URL:', URL);
+//     http.get(URL, function (req, res) {
+//         req.headers = {
+//             'Content-Type': 'application/x-www-form-urlencoded',
+//         }
+//         req.timeout = 30;
+//         var arr = [];
+//         req.on('data', function (chunk) {
+//             arr.push(chunk);
+//         });
+//         req.on('end', function () {
+//             var b = arr[0];
+//             for (var i = 1; i < arr.length; i++) {
+//                 b0 = Buffer.concat(b0, arr[i]);
+//             }
+//             console.log('b:', b, b.length, b.toString('hex'));
+//             var obj=infoparse.parse(b);
+//             console.log('obj:',obj);
+//             event.sender.send('replyinfo',obj);
+//         });
+//     });
+// })
+
+// ipcMain.on('utxo',function(event,data){
+//     // utxo(account, password, num, uock, address);
+//     // var addr = file.readAccount(account, password);
+//     // var uocks=5;
+//     // 1119AwBxBnRX3SdNM67EwPGb9CmSTUcP3qk7hhNVUuSGdXJGLjEnis
+//     // 1118Mi5XxqmqTBp7TnPQd1Hk9XYagJQpDcZu6EiGE1VbXHAw9iZGPV
+//     var URL = 'http://raw0.nb-chain.net/txn/state/uock?addr=1119AwBxBnRX3SdNM67EwPGb9CmSTUcP3qk7hhNVUuSGdXJGLjEnis&num=5&uock2=[]'
+//     http.get(URL, function (req, res) {
+//         req.headers = {
+//             'Content-Type': 'application/x-www-form-urlencoded',
+//         }
+//         req.timeout = 30;
+//         var arr = [];
+//         req.on('data', function (chunk) {
+//             arr.push(chunk);
+//         });
+//         req.on('end', function () {
+//             var b = arr[0];
+//             for (var i = 1; i < arr.length; i++) {
+//                 b0 = Buffer.concat(b0, arr[i]);
+//             }
+//             console.log('b:', b, b.length, b.toString('hex'));
+//             var obj=utxoparse.parse(b);
+//             console.log('obj:',obj);
+//             event.sender.send('replyutxo',obj);
+//         });
+//     });
+
+//     console.log(data);
+// })
+
+// /**
+//  * test
+//  */
+// ipcMain.on('test', function (event, data) {
+//     console.log('data:', data);
+// })
+// ipcMain.on('test2', function (event, data) {
+//     console.log('data:', data);
+// })
